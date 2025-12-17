9173093d1303149383ae6b4bbb2499360a99a17a
CG-493 <caitlin.gardner.493@accesscreative.ac.uk>
2025-12-17 16:58:56 +0000

    powerup 0.1
    spawner mostly works

--- TimeAttackGameJam/TimeAttackGameJam/Assets/Scripts/PowerUpScript.cs
+++ TimeAttackGameJam/TimeAttackGameJam/Assets/Scripts/PowerUpScript.cs
@@ -2,15 +2,68 @@ using UnityEngine;
 
 public class PowerUpScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Spawner Information")]
+    [SerializeField] bool hasSpawned;
+    [SerializeField] GameObject[] powerUpArray;
+    [SerializeField] GameObject lastSpawn;
+
+    [Header("Spawner Randomisation")]
+    [SerializeField] float randX;
+    [SerializeField] float randY;
+    [SerializeField] Vector3 randLocation;
+
+    [Header("Spawner Cooldown")]
+    [SerializeField] float cooldownTimer;
+  
+    
     void Start()
     {
-        
+        EventManager.current.GameStart += onGameStart;
     }
 
-    // Update is called once per frame
+    public void onGameStart()
+    {
+        hasSpawned = false;
+    }
+
+
     void Update()
     {
-        
+        if (!hasSpawned)
+        {
+            SpawnPowerUp();
+        }
+
+
+        if (hasSpawned)
+        {
+            SpawnCooldown();
+        }
+
+
+    }
+
+
+    void SpawnPowerUp()
+    {
+        randY = Random.Range(-6f, 6f);
+        randX = Random.Range(-11f, 11f);
+        randLocation = new Vector3(randY, 0, randX);
+
+        int randPower = Random.Range(0, powerUpArray.Length);
+        Instantiate(powerUpArray[randPower], randLocation, Quaternion.identity);
+
+        hasSpawned = true;
+    }
+
+    void SpawnCooldown()
+    {
+        cooldownTimer -= Time.deltaTime;
+        if (cooldownTimer <= 0)
+        {
+            hasSpawned = false;
+        }
+
     }
 }
+
